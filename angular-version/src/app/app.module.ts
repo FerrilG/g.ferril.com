4d6a5a9ba54a7fa75cb9bda433589084ef244299
Geoffrey Ferril <geoffferril@texasmutual.com>
2020-06-21 14:59:10 -0500

    created foundation for new angular site

---
+++ angular-version/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
+import { HeaderComponent } from './core/header/header/header.component';
+import { FooterComponent } from './core/footer/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
