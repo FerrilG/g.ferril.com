9a9e5b43029d3dc002932aa3798099eaa2f926d8
Geoffrey Ferril <geoffferril@texasmutual.com>
2020-06-21 21:53:05 -0500

    Setup project array

---
+++ angular-version/src/app/config/projects.config.ts
@@ -0,0 +1,12 @@
+export interface ProjectProp {
+    name: string;
+    number: number;
+    enabled: boolean;
+    lock: boolean;
+    folder: string;
+    role: string;
+    type: string;
+    company: string;
+    cover: string;
+    overview: string;
+  }
---
+++ angular-version/src/app/core/mocks/project.mock.ts
@@ -0,0 +1,28 @@
+import { ProjectProp } from 'src/app/config/projects.config';
+
+export const ProjectList: ProjectProp[] = [
+    {
+        name: 'Portfolio',
+        number: 1,
+        enabled: true,
+        lock: false,
+        folder: 'string',
+        role: 'string',
+        type: 'string',
+        company: 'string',
+        cover: 'string',
+        overview: 'string'
+    },
+    {
+        name: 'Design System',
+        number: 1,
+        enabled: true,
+        lock: false,
+        folder: 'string',
+        role: 'string',
+        type: 'string',
+        company: 'string',
+        cover: 'string',
+        overview: 'string'
+    }
+];
