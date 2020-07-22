7f1df4cfbde70496fa5534862cd1f1f36c3cba8f
Geoffrey Ferril <geoffferril@texasmutual.com>
2020-07-22 17:57:00 -0500

    code fix

--- angular-version/src/app/core/header/header.module.ts
+++ angular-version/src/app/core/header/header.module.ts
@@ -1,7 +1,7 @@
 import { HeaderTabComponent } from './header-tab.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
 @NgModule({
     declarations: [
